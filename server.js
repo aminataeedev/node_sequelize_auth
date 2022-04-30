16e798fcfb4e1833649793dc68e9d35936c65bcc
AminAtaee <maminataee@gmail.com>
2022-05-01 04:15:24 +0430

    Validation error fixed

--- server.js
+++ server.js
@@ -17,23 +17,7 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
 app.use("/auth", authRoutes);
-
-app.use((err, req, res, next) => {
-    console.log(err);
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
-    res.status(err.statusCode).json({
-        message: err.message,
-    });
-});
 
 User.sync({ force: true }).then(() => {
     app.listen(port, () => {
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,29 +1,39 @@
 const User = require("../models/user");
 const jwtHelpers = require("../helpers/jwt");
 const bcryptHelpers = require("../helpers/bcrypt");
-const { requestValidator } = require("../helpers/errorHandler");
+const { validationResult } = require("express-validator");
 
 const register = async (req, res, next) => {
-    const { firstname, lastname, email, password } = req.body;
-    requestValidator(req, res);
-    const findUser = await User.findOne({ where: { email } });
-    if (findUser) {
-        return res.status(409).json({ status: "failed", message: "This email has already been registered." });
+    try {
+        const { firstname, lastname, email, password } = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const findUser = await User.findOne({ where: { email } });
+        if (findUser) {
+            return res.status(409).json({ status: "failed", message: "This email has already been registered." });
+        }
+        const encryptedPassword = await bcryptHelpers.encrypt(password, 10);
+        const user = await User.create({
+            firstname,
+            lastname,
+            email,
+            password: encryptedPassword,
+        });
+        const token = await jwtHelpers.tokenMaker(user.email);
+        user.token = token;
+        return res.status(201).json(user);
+    } catch (err) {
+        console.log("---", err);
     }
-    const encryptedPassword = await bcryptHelpers.encrypt(password, 10);
-    const user = await User.create({
-        firstname,
-        lastname,
-        email,
-        password: encryptedPassword,
-    });
-    const token = await jwtHelpers.tokenMaker(user.email);
-    user.token = token;
-    return res.status(201).json(user);
 };
 const signIn = async (req, res, next) => {
     const { email, password } = req.body;
-    requestValidator(req, res);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const findUser = await User.findOne({ where: { email: email } });
     if (!findUser) return res.status(404).json({ status: "failed", message: "Email or password is wrong" });
     const comparePassword = await bcryptHelpers.compare(password, findUser.password);
